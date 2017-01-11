f7341953046179010d938d941cae659a5162cffd
Paddy Moran <pdmoran@outlook.com>
2017-01-11 20:21:11 +0000

    Moved odt creation to JS (no longer using shell commands)

--- app/server.js
+++ app/server.js
@@ -3,64 +3,102 @@ var bodyParser = require('body-parser');
 var Promise = require('bluebird');
 const fs = Promise.promisifyAll(require("fs"));
 const nunjucks = require('nunjucks')
-const AdmZip = require('adm-zip');
-
+var JSZip = require("jszip");
 
 var app = express();
 app.use(bodyParser.json());
+
+nunjucks.configure('templates', { autoescape: true });
+
+const DEFAULT_BASE_DOCUMENT = 'base_documents/default.odt';
 
 module.exports = function(config) {
     const port = config.server_port || 3000;
 
     app.get('/', function (request, response) {
-        const data = {
-            company: {
-                companyName: "CATALEX LIMITED",
-                companyNumber: 366352
-            },
-            resolutionOptions: {
-                resolutionType: 'Resolution at Board Meeting',
-                dateOfMinute: '2015-10-08',
-                chairperson: {
-                    name: 'Testing Chairperson Name'
-                }
-            },
-            resolution: {
-                dateOfBoardMeeting: '2015-09-31'
-            },
-            resolutions: [{
-                individualResolutionType: 'Custom',
-                resolutionOptions: {
-                    text: 'testing testing testing one two three'
-                }
-            },{
-                individualResolutionType: 'Change Company Name',
-                resolutionOptions: {
-                    newCompanyName: 'NEW COMPANY NAME',
-                    effectiveDate: '2016-05-04'
-                }
-            },{
-                individualResolutionType: 'Records in Good Companies'
-            },{
-                individualResolutionType: 'Agent for Company Changes',
-                resolutionOptions: {
-                    nameOfAuthorisedAgent: 'Paddy Moran'
-                }
-            }],
-            notes: [
-                'one two three four',
-                'lorem ipsum'
-            ]
-        };
+        response.end('done\n');
+    });
 
-        nunjucks.configure('templates', { autoescape: true });
-        const xml = nunjucks.render('board_resolution.njk', data);
+    app.get('/test', function (request, response) {
+        
 
-        fs.writeFileSync('./tmp/out.xml', xml, 'utf8');
+        packZip(renderTemplate());
+
+        // var templateOdt = new AdmZip('./template_odt.odt');
+
+
+
+        // var zip = new AdmZip();
+        // const zipEntries = zip.getEntries();
+
+        // zip.addFile('contents.xml', new Buffer('inner content of the file'), 'entry comment goes here');
+        // const completeFile = zip.toBuffer((buffer) => console.log(buffer), (error) => console.log(error));
+        // // zip.writeZip('./done.odt');
+
         response.end('done\n');
     });
 
     app.listen(port, function () {
         console.log('Node server running at localhost:' + port);
     });
+}
+
+function packZip(contentXml) {
+    return fs.readFileAsync(DEFAULT_BASE_DOCUMENT)
+        .then((odt) => {
+            return JSZip.loadAsync(odt)
+                .then((zip) => {
+                    zip.file('content.xml', contentXml);
+
+                    return zip.generateAsync({type: 'nodebuffer'})
+                        .then((odtBuffer) => {
+                            fs.writeFileSync('test.odt', odtBuffer);
+                        });
+                });
+        })
+        .catch(error => console.log(error));
+}
+
+function renderTemplate() {
+    const data = {
+        company: {
+            companyName: "CATALEX LIMITED",
+            companyNumber: 366352
+        },
+        resolutionOptions: {
+            resolutionType: 'Resolution at Board Meeting',
+            dateOfMinute: '2015-10-08',
+            chairperson: {
+                name: 'Testing Chairperson Name'
+            }
+        },
+        resolution: {
+            dateOfBoardMeeting: '2015-09-31'
+        },
+        resolutions: [{
+            individualResolutionType: 'Custom',
+            resolutionOptions: {
+                text: 'testing testing testing one two three'
+            }
+        },{
+            individualResolutionType: 'Change Company Name',
+            resolutionOptions: {
+                newCompanyName: 'NEW COMPANY NAME',
+                effectiveDate: '2016-05-04'
+            }
+        },{
+            individualResolutionType: 'Records in Good Companies'
+        },{
+            individualResolutionType: 'Agent for Company Changes',
+            resolutionOptions: {
+                nameOfAuthorisedAgent: 'Paddy Moran'
+            }
+        }],
+        notes: [
+            'one two three four',
+            'lorem ipsum'
+        ]
+    };
+
+    return nunjucks.render('board_resolution.njk', data);
 }
--- repack_odt.sh
+++
@@ -1,5 +0,0 @@
-cd ./tmp/repack_work_area
-cp ../out.xml ./content.xml
-
-rm .DS_Store # IMPORTANT - ODT will be corrupt if one of these exists
-zip -r ../finished.odt * -x mimetype # -r = recursive, -x mimetype = exclude mimetype file
