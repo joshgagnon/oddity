e32b78522bdcaec382223ee1488a60f7d10c7297
Paddy Moran <pdmoran@outlook.com>
2017-01-11 20:39:49 +0000

    Send odt back as part of request instead of saving it to disk

--- app/server.js
+++ app/server.js
@@ -16,26 +16,15 @@ module.exports = function(config) {
     const port = config.server_port || 3000;
 
     app.get('/', function (request, response) {
-        response.end('done\n');
     });
 
     app.get('/test', function (request, response) {
-        
-
-        packZip(renderTemplate());
-
-        // var templateOdt = new AdmZip('./template_odt.odt');
-
-
-
-        // var zip = new AdmZip();
-        // const zipEntries = zip.getEntries();
-
-        // zip.addFile('contents.xml', new Buffer('inner content of the file'), 'entry comment goes here');
-        // const completeFile = zip.toBuffer((buffer) => console.log(buffer), (error) => console.log(error));
-        // // zip.writeZip('./done.odt');
-
-        response.end('done\n');
+        const renderedContentXml = nunjucks.render('board_resolution.njk', testData);
+        packZip(renderedContentXml).then((zip) => {
+            response.set('Content-Type', 'application/zip')
+            response.set('Content-Disposition', 'attachment; filename=file.zip');
+            response.end(zip, 'binary');
+        });
     });
 
     app.listen(port, function () {
@@ -49,56 +38,48 @@ function packZip(contentXml) {
             return JSZip.loadAsync(odt)
                 .then((zip) => {
                     zip.file('content.xml', contentXml);
-
-                    return zip.generateAsync({type: 'nodebuffer'})
-                        .then((odtBuffer) => {
-                            fs.writeFileSync('test.odt', odtBuffer);
-                        });
+                    return zip.generateAsync({type: 'nodebuffer'});
                 });
         })
         .catch(error => console.log(error));
 }
 
-function renderTemplate() {
-    const data = {
-        company: {
-            companyName: "CATALEX LIMITED",
-            companyNumber: 366352
-        },
+const testData = {
+    company: {
+        companyName: "CATALEX LIMITED",
+        companyNumber: 366352
+    },
+    resolutionOptions: {
+        resolutionType: 'Resolution at Board Meeting',
+        dateOfMinute: '2015-10-08',
+        chairperson: {
+            name: 'Testing Chairperson Name'
+        }
+    },
+    resolution: {
+        dateOfBoardMeeting: '2015-09-31'
+    },
+    resolutions: [{
+        individualResolutionType: 'Custom',
         resolutionOptions: {
-            resolutionType: 'Resolution at Board Meeting',
-            dateOfMinute: '2015-10-08',
-            chairperson: {
-                name: 'Testing Chairperson Name'
-            }
-        },
-        resolution: {
-            dateOfBoardMeeting: '2015-09-31'
-        },
-        resolutions: [{
-            individualResolutionType: 'Custom',
-            resolutionOptions: {
-                text: 'testing testing testing one two three'
-            }
-        },{
-            individualResolutionType: 'Change Company Name',
-            resolutionOptions: {
-                newCompanyName: 'NEW COMPANY NAME',
-                effectiveDate: '2016-05-04'
-            }
-        },{
-            individualResolutionType: 'Records in Good Companies'
-        },{
-            individualResolutionType: 'Agent for Company Changes',
-            resolutionOptions: {
-                nameOfAuthorisedAgent: 'Paddy Moran'
-            }
-        }],
-        notes: [
-            'one two three four',
-            'lorem ipsum'
-        ]
-    };
-
-    return nunjucks.render('board_resolution.njk', data);
-}
+            text: 'testing testing testing one two three'
+        }
+    },{
+        individualResolutionType: 'Change Company Name',
+        resolutionOptions: {
+            newCompanyName: 'NEW COMPANY NAME',
+            effectiveDate: '2016-05-04'
+        }
+    },{
+        individualResolutionType: 'Records in Good Companies'
+    },{
+        individualResolutionType: 'Agent for Company Changes',
+        resolutionOptions: {
+            nameOfAuthorisedAgent: 'Paddy Moran'
+        }
+    }],
+    notes: [
+        'one two three four',
+        'lorem ipsum'
+    ]
+};
