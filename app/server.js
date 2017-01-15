c25ab3f16b379bbabc414b8a2d844e478658819a
Paddy Moran <pdmoran@outlook.com>
2017-01-15 21:18:38 +0000

    Added some filters

--- app/server.js
+++ app/server.js
@@ -15,6 +15,55 @@ env.addFilter('date', function(date) {
     return moment(date, 'YYYY-MM-DD').format('D MMM YYYY');
 });
 
+// Take a list of items and format them in a string
+// item one
+// item one and item two
+// item one, item two, and item three
+env.addFilter('join_and', function(items) {
+    let result = '';
+
+    // If two items, just return them with the word and in-between
+    if (items.length == 2) {
+        return items[0] + ' and ' + items[1];
+    }
+
+    items.map((item, index) => {
+        // Last item needs an 'and' before it
+        if (index == 0) {
+            result = item;
+        } if ((index - 1) != items.length) {
+            result += ', ' + item;
+        } else {
+            result += ', and ' + item;
+        }
+    });
+
+    return result;
+});
+
+// Take a list of objects and turn flatten it based on a single attribute
+// flatten([{name: 'Tom'}, {name: 'Dick'}, {name: 'Harry'}], 'name')
+// == ['Tom', 'Dick', 'Harry']
+env.addFilter('flatten', function(list, attribute) {
+    list.reduce((a, b) => {
+        if (a[attribute]) {
+            return [a[attribute]].concat(b[attribute]);
+        }
+
+        return a.concat(b[attribute]);
+    });
+});
+
+env.addFilter('exists', function(list, attribute, value) {
+    for (let index = 0; index < list.length; index++) {
+        if (item[index][attribute] && item[index][attribute] == value) {
+            return true;
+        }
+    }
+
+    return false;
+});
+
 const DEFAULT_BASE_DOCUMENT = 'base_documents/default.odt';
 
 const tempaltes = [
