1df696b11b5aa75f8eb39a4b1f8e807955295a76
Joshua Gagnon <josh.n.gagnon@gmail.com>
2018-03-16 15:02:58 +1100

    updated the update script

--- update.sh
+++ update.sh
@@ -2,5 +2,6 @@ git pull
 rm -r node_modules/good-companies-templates/
 rm -r node_modules/court-costs-templates/
 rm -r node_modules/el-templates/
+rm -r node_modules/anthony-harper-templates/
 npm install
 pm2 restart oddity_production
