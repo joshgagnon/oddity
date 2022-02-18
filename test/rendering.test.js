0e4fac1a24960c55ebab8c74790c3f0ee614b152
Josh Gagnon <josh.n.gagnon@gmail.com>
2022-02-18 14:38:49 +1100

    tweakies

--- test/rendering.test.js
+++ test/rendering.test.js
@@ -9,57 +9,60 @@ const libxmljs = require("libxmljs");
 const JSZip = require("jszip");
 const os = require('os');
 
-describe('Test rendering for schemas with test data', function() {
+describe('Test rendering for schemas with test data', function () {
     let envs, schemas, schemaMap;
-    before('prep nunjucks and schemas', function() {
-       return getEnvironments()
-        .then(_envs => {
-            envs = _envs;
-            return fs.readFileAsync(path.join(__dirname, '../environments.json'))
-        })
-        .then(data => {
-            const obj = JSON.parse(data);
-            schemas = Object.keys(obj).map(key => obj[key]);
-            schemaMap = Object.keys(obj).reduce((acc, key) => {
-                acc[obj[key]] = key;
-                return acc;
-            }, {})
-        });
+    before('prep nunjucks and schemas', function () {
+        return getEnvironments()
+            .then(_envs => {
+                envs = _envs;
+                return fs.readFileAsync(path.join(__dirname, '../environments.json'))
+            })
+            .then(data => {
+                const obj = JSON.parse(data);
+                schemas = Object.keys(obj).map(key => obj[key]);
+                schemaMap = Object.keys(obj).reduce((acc, key) => {
+                    acc[obj[key]] = key;
+                    return acc;
+                }, {})
+            });
     });
 
     it('reads test files', function () {
-        return Promise.map(schemas,  (schema) => {
+        return Promise.map(schemas, (schema) => {
             const testPath = path.join(__dirname, '../node_modules', schema, 'test_data');
             return fs.readdirAsync(testPath)
                 .then(schemaFolders => Promise.map(schemaFolders, folder =>
                     fs.readdirAsync(path.join(testPath, folder))
-                    .then(files =>  Promise.map(files, filename => {
-                        let zip;
-                        return fs.readFileAsync(path.join(testPath, folder, filename))
-                            .then(_data => {
-                                data = JSON.parse(_data)
-                                return render(envs[schemaMap[schema]], {values: data, formName: folder})
+                        .then(files => Promise.map(files, filename => {
+                                let zip;
+                                return fs.readFileAsync(path.join(testPath, folder, filename))
+                                    .then(_data => {
+                                        data = JSON.parse(_data)
+                                        return render(envs[schemaMap[schema]], {values: data, formName: folder})
+                                    })
+                                    .then(_zip => {
+                                        zip = _zip;
+                                        return JSZip.loadAsync(zip)
+                                    })
+                                    .then(content => {
+                                        return content.files["content.xml"].async('text')
+                                    })
+                                    .then(xml => {
+                                        // validate xml
+                                        return libxmljs.parseXml(xml);
+                                    })
+                                    .then(() => {
+                                        const tempPath = path.join(os.tmpdir(), folder + '-' + filename + '.odt');
+                                       //' console.log(tempPath);
+                                        return fs.writeFileAsync(tempPath, zip);
+                                    })
+                                    .catch(e => {
+                                        console.log("FAILED", folder, filename)
+                                        throw e;
+                                    })
                             })
-                            .then(_zip => {
-                                zip = _zip;
-                                return JSZip.loadAsync(zip)
-                            })
-                            .then(content => {
-                                return content.files["content.xml"].async('text')
-                            })
-                            .then(xml => {
-                                return libxmljs.parseXml(xml);
-                            })
-                            .then(() => {
-                                return fs.writeFileAsync(path.join(os.tmpdir(), folder+'-'+filename+'.odt'), zip);
-                            })
-                            .catch(e => {
-                                console.log("FAILED", folder, filename)
-                                throw e;
-                            })
-                        })
-                    )
+                        )
                 ))
-            })
+        })
     });
 });
